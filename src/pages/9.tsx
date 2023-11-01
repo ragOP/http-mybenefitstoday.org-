93f551facf68d104feabee9fb50255a391c356da
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2023-11-01 10:46:26 +0530

    d

--- src/pages/9.tsx
+++ src/pages/9.tsx
@@ -109,7 +109,7 @@ export default function Fifth_SP() {
     exit: "toast-exit",
   });
   const messages = [
-    "Velia E. Dunn from Salem, OR just Qualified For $6400 Allowance Card",
+    "Velia E. Dunn from Salem, OR just Qualified for $6400 Allowance Card",
     "Paul N. Parsons from Petersburg, VA just Qualified For $3200 Allowance Card.",
     "Paul N. Parsons from New Town, ND just Qualified For $3600 Allowance Card.",
     "Mary T. Pritts from Oakland, CA just Qualified For $4200 Allowance Card.",
